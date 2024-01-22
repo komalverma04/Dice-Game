07f2c1c26bc4f0d3917828444698fee3c2fb9e29
komalverma04 <114138604+komalverma04@users.noreply.github.com>
2024-01-22 21:47:33 +0530

    Add files via upload
    Added JS, CSS and HTML file for the dice game.

---
+++ index.js
@@ -0,0 +1,13 @@
+var random1 = Math.floor(Math.random() * 6) + 1;
+var random2 = Math.floor(Math.random() * 6) + 1;
+var randomimage1 = "images/dice" + random1 + ".png";
+var randomimage2 = "images/dice" + random2 + ".png";
+document.querySelectorAll("img")[0].src = randomimage1;
+document.querySelectorAll("img")[1].src = randomimage2;
+if(random1 > random2){
+    document.querySelector(".Result").innerHTML = "Player 1  WON!!";
+}else if(random1 < random2){
+    document.querySelector(".Result").innerHTML = "Player 2  WON!!";
+}else{
+    document.querySelector(".Result").innerHTML = "DRAW!!";
+}
